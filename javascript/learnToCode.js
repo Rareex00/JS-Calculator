4586d6b8391cb3bfd4efdef845c32f1cb4cf6fcd
Chihaia Rares <chihaiarares@gmail.com>
2020-11-19 23:07:42 +0200

    1st update

---
+++ javascript/learnToCode.js
@@ -0,0 +1,31 @@
+//var students = [];
+//
+//function Student (firstName, lastName, age) {
+//    this.firstName = firstName;
+//    this.lastName = lastName;
+//    this.age = age;
+//    this.greeting = function() {
+//        return "Hai noroc, ma cheama " + this.firstName + " si am " + this.age + " ani.";
+//    }
+//} 
+//
+//var s1 = new Studenttudent("Ion", "Animalu", 34);
+//
+//console.log(s1);
+//console.log(s1.greeting());
+//
+//students.push(new Student("Ion", "Animalu", 34));
+//students.push(new Student("Gion", "Calu", 22));
+//students.push(new Student("Paducel", "Popa", 36));
+//students.push(new Student("Panda", "Frumi", 41));
+//
+//console.log(students);
+//console.log(students.length);
+//var len = students.length;
+//for (var i = 0; i < len; i++){
+//    var student = students[i];
+//    console.log(student.greeting());
+//}
+
+
+
